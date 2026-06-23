762141083682d0cf5e918dd999903368e5413c1d
sagarrajgure20-a11y <sagarrajgure20@gmail.com>
2026-06-23 12:37:55 +0530

    update

--- cucumber.js
+++ cucumber.js
@@ -1,8 +1,18 @@
-export default {
-   default: {
-    require: ["Features-bbd/step-definations/**/*.js"],
-    paths: ["Features-bbd/**/*.feature"],
-    format: ["progress"],
-    publishQuiet: true,
-  },
-};
+import { format } from "node:path";
+
+module.exports = {
+    default: {
+        require:['Features-bbd/step-definations/*.js'],
+        format: ['progress', 'html:reports/cucumber-report.html']
+    }
+}
+
+
+// export default {
+//    default: {
+//     require: ["Features-bbd/step-definations/**/*.js"],
+//     paths: ["Features-bbd/**/*.feature"],
+//     format: ["progress"],
+//     publishQuiet: true,
+//   },
+// };
