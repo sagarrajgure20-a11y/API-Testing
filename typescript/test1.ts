fee75137a6b122a0ed17d6d91b5e4e0d3b323d78
sagarrajgure20-a11y <sagarrajgure20@gmail.com>
2026-06-26 15:14:34 +0530

    Update

--- typescript/test1.ts
+++ typescript/test1.ts
@@ -2,3 +2,14 @@
 let a:number = 10;
 a = 20;
 console.log(a);
+
+let b: number|string = 10;
+b = "Sagar";
+b = 50.23265;
+console.log(b);
+
+let c: any = 10;
+c = "Sagar";
+c = false;
+console.log(c);
+
