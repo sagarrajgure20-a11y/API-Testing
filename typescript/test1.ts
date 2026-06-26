ca5bc39356fd3a0b3f196d60ddb89eb1ae463332
sagarrajgure20-a11y <sagarrajgure20@gmail.com>
2026-06-26 15:37:07 +0530

    update

--- typescript/test1.ts
+++ typescript/test1.ts
@@ -13,3 +13,28 @@ c = "Sagar";
 c = false;
 console.log(c);
 
+
+// Function
+function add(d:number, e:number): number{
+    let f:number = d + e;
+    return f;
+}
+
+console.log(add(10, 20));
+
+// Interface
+interface Student{
+    name: string;
+    age: number;
+}
+
+let std1:Student = {
+    "name": "Sagar",
+    "age": 32
+    
+}
+console.log(std1);
+
+// Tuple array
+
+let arr: [number, string] = [10, "Sagar"];  // Fix size
